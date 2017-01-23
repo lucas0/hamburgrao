446720811ab92ea459fe901f447c089b5d5188f4
Lucas Azevedo <lucas.ccomp@gmail.com>
2017-01-23 17:20:43 +0000

    nodemon and git added

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
-// server.js
-// import the http module
-var http = require('http');
+// // server.js
+// // import the http module
+// var http = require('http');
 
-// handle sending requests and returning responses
-function handleRequests(req, res) {
-  // return string
-  res.end('Hello world!');
-}
+// // handle sending requests and returning responses
+// function handleRequests(req, res) {
+//   // return string
+//   res.end('Hello world!');
+// }
 
-// create the server
-var server = http.createServer(handleRequests);
+// // create the server
+// var server = http.createServer(handleRequests);
 
-// start server and listen on port x
-server.listen(8080, function() {
-  console.log('Listening on port 8080');
+// // start server and listen on port x
+// server.listen(8080, function() {
+//   console.log('Listening on port 8080');
+// });
+
+ // server.js
+var express = require('express');
+var app = express();
+var port = 8080;
+
+// start the server
+app.listen(port, function() {
+  console.log('app started');
+});
+
+// route our app
+app.get('/', function(req, res) {
+  res.send('hello woasdarld!');
 });
